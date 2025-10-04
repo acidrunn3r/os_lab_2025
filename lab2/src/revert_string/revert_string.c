e4597cdfb9929fddf28eba58decd9c06a3423d2b
Alexander Lipov <142092955+acidrunn3r@users.noreply.github.com>
2025-10-04 10:32:36 +0000

    Add revert string function

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    int i, j;
+    
+    for (i = 0, j = length - 1; i < j; i++, j--)
+    {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
+//gcc -o revert_string main.c revert_string.c
